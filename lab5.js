a7f66c42226126586b101a41d9b36e46864a4bc2
katteremenko <katt.eremenko@gmail.com>
2021-10-30 14:34:36 +0700

    add

---
+++ lab5.js
@@ -0,0 +1,24 @@
+
+function open1() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description1').style.display = 'block';
+}
+
+function open2() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description2').style.display = 'block';
+}
+
+function open3() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description3').style.display = 'block';
+}
